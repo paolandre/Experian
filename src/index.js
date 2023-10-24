76a43bd1a5ecdc394a29bc702d8069eb0b9cf0a5
Paola Andrea Ramirez <129551206+paolandre@users.noreply.github.com>
2023-10-24 13:04:48 -0500

    Cambio de vistas y componentes, creación de la tabla de tab

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/views/home/home";
-import PersonalFow from "./components/views/personalFow/personalFow";
-import TeamFow from "./components/views/teamFow/teamFow";
-import EmployeeFow from "./components/views/employeeFow/employeeFow";
+import LeaderView from "./components/views/leaderView/leaderView";
+import EmployeeView from "./components/views/employeeView/employeeView";
 import "./index.css"
 
 const router = createBrowserRouter([
@@ -13,16 +12,12 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: "/personal-fow",
-    element: <PersonalFow />,
+    path: "/leader-view",
+    element: <LeaderView />,
   },
   {
-    path: "/team-fow",
-    element: <TeamFow />
-  },
-  {
-    path: "/employee-fow",
-    element: <EmployeeFow />
+    path: "/employee-view",
+    element: <EmployeeView />
   }
 ]);
 
