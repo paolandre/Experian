1a178f4fddae1d190e7524eb606c5ab06f78719c
Paola Andrea Ramirez <129551206+paolandre@users.noreply.github.com>
2023-10-24 12:00:48 -0500

    paolandre componente de la barra de navegación, falta el logo

--- src/components/views/personalFow/personalFow.jsx
+++ src/components/views/personalFow/personalFow.jsx
@@ -1,21 +1,51 @@
 import "./personalFow.css";
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import React from 'react';
+import HeaderComponent from '../../commonComponents/navbar/navBar.jsx';
 
 export default function PersonalFow() {
-
     return (
-        <>
+        <div>
             <header>
+                <HeaderComponent />
             </header>
 
-            <main>
-                <h1>Personal Fow!</h1>
-            </main>
+            <div className="flex items-center space-x-1 ml-3 p-1 my-1.5">
+                <AiOutlineArrowLeft className='text-dark-blue' style={{ width: '20px', height: '20px' }} />
+                <h1 className='text-dark-blue px-1 mb-1'>Home</h1>
+            </div>
+
+
+            <div>
+                <span>Personal FOW</span>
+                <span>Team FOW</span>
+            </div>
+
+            <div>
+                <span>Office Entrance</span>
+                <span>Authorized Exceptions</span>
+                <span>Holiday</span>
+                <span>Scheduled Absences</span>
+            </div>
+
+            <div>
+                <h2>Leader Name</h2>
+                <p>10:72794037</p>
+                <p>Company: Comb e</p>
+                <p>Other Information: Some placeholder text here</p>
+            </div>
+
+            <div>
+                <h2>Calendar Summary</h2>
+                <p>Scheduled Exceptions</p>
+                <p>Scheduled Absences</p>
+            </div>
 
             <footer>
+                <p>If you have any concerns or need clarification please feel free to contact us at the email address:</p>
+                <a href="mailto:somehrsia@experian.com">somehrsia@experian.com</a>
             </footer>
-        </>
-    )
+        </div>
+    );
 }
-
-
 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        'custom-gray': '#717171',
+        'dark-blue': '#426DA9'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
