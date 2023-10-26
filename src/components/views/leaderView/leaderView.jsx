794e90972870f93d821b44916e7ab1429a338a56
Andrea Roa <andrearoa182@gmail.com>
2023-10-25 21:38:17 -0500

    se agrega efectos y tabla al teamFow

--- src/components/views/leaderView/leaderView.jsx
+++ src/components/views/leaderView/leaderView.jsx
@@ -1,63 +1,83 @@
 import "./leaderView.css";
-import { AiOutlineArrowLeft } from 'react-icons/ai';
-import HeaderComponent from '../../commonComponents/navbar/navBar.jsx';
-import React, { useState } from 'react';
-import PersonalFowTab from '../../commonComponents/personalFowTab/personalFow';
-import TeamFowTab from '../../commonComponents/TeamFowTab/teamFow';
-import { Link } from 'react-router-dom';
-import TeamAbsences from './../../commonComponents/teamAbsences/teamAbsences';
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import HeaderComponent from "../../commonComponents/navbar/navBar.jsx";
+import React, { useState } from "react";
+import PersonalFowTab from "../../commonComponents/personalFowTab/personalFow";
+import TeamFowTab from "../../commonComponents/TeamFowTab/teamFow";
+import { Link } from "react-router-dom";
+import TeamAbsences from "./../../commonComponents/teamAbsences/teamAbsences";
 
 export default function LeaderView() {
-    const [activeTab, setActiveTab] = useState('Personal FOW');
-    return (
-        <div className='bg-light-pink pb-8'>
-            <header>
-                <HeaderComponent />
-            </header>
+  const [activeTab, setActiveTab] = useState("Personal FOW");
+  return (
+    <div className="bg-light-pink pb-8">
+      <header>
+        <HeaderComponent />
+      </header>
 
-            {/* Comienza el tab bar */}
-            <div className="flex items-center space-x-1 ml-3 p-1 my-1.5 rounded-md ">
-                <Link to="/">
-                    <div className="flex items-center space-x-1 ml-3 p-1 my-1.5 rounded-md ">
-                        <AiOutlineArrowLeft className='text-dark-blue' style={{ width: '20px', height: '20px' }} />
-                        <h1 className='text-dark-blue px-1 mb-1'>Home</h1>
-                    </div>
-                </Link>
-            </div>
+      {/* Comienza el tab bar */}
+      <div className="flex items-center space-x-1 ml-3 p-1 my-1.5 rounded-md ">
+        <Link to="/">
+          <div className="flex items-center space-x-1 ml-3 p-1 my-1.5 rounded-md ">
+            <AiOutlineArrowLeft
+              className="text-dark-blue"
+              style={{ width: "20px", height: "20px" }}
+            />
+            <h1 className="text-dark-blue px-1 mb-1">Home</h1>
+          </div>
+        </Link>
+      </div>
 
-            <div className="ml-10 bg-background-gray shadow-custom-shadow  p-4 mr-10 rounded-2xl relative">
-                <span
-                    className={`cursor-pointer ${activeTab === 'Personal FOW' ? 'border-b-2 border-pink-color font-bold text-dark-blue' : 'text-black'}`}
-                    onClick={() => setActiveTab('Personal FOW')}>
-                    Personal FOW
-                </span>
+      <div className="ml-10 bg-background-gray shadow-custom-shadow  p-4 mr-10 rounded-2xl relative ">
+        <span
+          className={`cursor-pointer ${
+            activeTab === "Personal FOW"
+              ? "border-b-2 border-pink-color font-bold text-dark-blue"
+              : "text-black"
+          }`}
+          onClick={() => setActiveTab("Personal FOW")}
+        >
+          Personal FOW
+        </span>
 
-                <span
-                    className={`ml-4 cursor-pointer ${activeTab === 'Team FOW' ? 'border-b-2 border-pink-color font-bold text-dark-blue' : 'text-black'}`}
-                    onClick={() => setActiveTab('Team FOW')}>
-                    Team FOW
-                </span>
+        <span
+          className={`ml-4 cursor-pointer ${
+            activeTab === "Team FOW"
+              ? "border-b-2 border-pink-color font-bold text-dark-blue"
+              : "text-black"
+          }`}
+          onClick={() => setActiveTab("Team FOW")}
+        >
+          Team FOW
+        </span>
 
-                <span
-                    className={`ml-4 cursor-pointer ${activeTab === 'Team Absences' ? 'border-b-2 border-pink-color font-bold text-dark-blue' : 'text-black'}`}
-                    onClick={() => setActiveTab('Team Absences')}>
-                    Team Absences
-                </span>
+        <span
+          className={`ml-4 cursor-pointer ${
+            activeTab === "Team Absences"
+              ? "border-b-2 border-pink-color font-bold text-dark-blue"
+              : "text-black"
+          }`}
+          onClick={() => setActiveTab("Team Absences")}
+        >
+          Team Absences
+        </span>
 
-                <span
-                    className={`ml-4 cursor-pointer ${activeTab === 'Absences Record' ? 'border-b-2 border-pink-color font-bold text-dark-blue' : 'text-black'}`}
-                    onClick={() => setActiveTab('Absences Record')}>
-                    Absences Record
-                </span>
+        <span
+          className={`ml-4 cursor-pointer ${
+            activeTab === "Absences Record"
+              ? "border-b-2 border-pink-color font-bold text-dark-blue"
+              : "text-black"
+          }`}
+          onClick={() => setActiveTab("Absences Record")}
+        >
+          Absences Record
+        </span>
 
-                {/* Condicionales para las dos tabs */}
-                {activeTab === 'Personal FOW' && <PersonalFowTab />}
-                {activeTab === 'Team FOW' && <TeamFowTab />}
-                {activeTab === 'Team Absences' && <TeamAbsences />}
-            </div>
-
-        </div>
-    );
+        {/* Condicionales para las dos tabs */}
+        {activeTab === "Personal FOW" && <PersonalFowTab />}
+        {activeTab === "Team FOW" && <TeamFowTab />}
+        {activeTab === "Team Absences" && <TeamAbsences />}
+      </div>
+    </div>
+  );
 }
-
-
